750f55f395dd13562b1b0d83a9a801c1748738e2
ymdarake <t.sakamoto.ouen@gmail.com>
2020-03-10 17:43:23 +1300

    [feat]2.4: Return Statement and String() method

--- parser/parser.go
+++ parser/parser.go
@@ -63,6 +63,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -82,6 +84,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+	// TODO: We're skipping the expressions until we encounter a semicoln
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
