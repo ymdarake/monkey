b81a48d8ce6fab7daa2ad2647c43d0362e2580af
ymdarake <t.sakamoto.ouen@gmail.com>
2020-03-23 18:50:04 +1300

    [3.1-3.]Evaluator and Objects

---
+++ evaluator/evaluator.go
@@ -0,0 +1,30 @@
+package evaluator
+
+import (
+	"github.com/ymdarake/monkey/ast"
+	"github.com/ymdarake/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
