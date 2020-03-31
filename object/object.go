d1d04fc4a93627c64337237df7dca22838f4406e
ymdarake <t.sakamoto.ouen@gmail.com>
2020-03-31 13:30:41 +1300

    [3.10]Functions & Function Calls

--- object/object.go
+++ object/object.go
@@ -1,13 +1,20 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/ymdarake/monkey/ast"
+)
 
 const (
 	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
+	ERROR_OBJ        = "ERROR"
 )
 
 type ObjectType string
@@ -51,6 +58,7 @@ func (n *Null) Inspect() string {
 type ReturnValue struct {
 	Value Object
 }
+
 func (rv *ReturnValue) Type() ObjectType {
 	return RETURN_VALUE_OBJ
 }
@@ -58,9 +66,36 @@ func (rv *ReturnValue) Inspect() string {
 	return rv.Value.Inspect()
 }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType {
+	return FUNCTION_OBJ
+}
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(f.Body.String())
+
+	return out.String()
+}
+
 type Error struct {
 	Message string
 }
+
 func (e *Error) Type() ObjectType {
 	return ERROR_OBJ
 }
